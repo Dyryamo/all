736ad84f6ffbe7b3b7787762fb72cf7e9f056ddc
Mr zhong <519233982@qq.com>
2018-01-18 16:04:47 +0800

    2018/1/18

---
+++ learnpython/wen_jian_chong_minming_ji_changjian_wnejian_jia.py
@@ -0,0 +1,10 @@
+#import os包
+#os.rename()//先是源文件名，后是新文件名
+#os.remove()//删除文件
+#os.mkdir() //创建文件夹
+#os.rmdir()//删除文件夹
+#osgetcwd()//获取路径,返回当前操作的绝对路径
+#os.chdir()//改变默认目录
+#os.listdir()//列出当前目录
+os.rename("xxx.txt","yyy.txt")
+os.remove("yyy.txt")
